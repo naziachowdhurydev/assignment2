721bae4d3d442ab227aef7bd8ecf1246894ecfec
naziachowdhurydev <naziachowdhurydev@gmail.com>
2025-02-08 16:15:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const firstName = document.querySelector('#fname');
+// console.log(firstName);
+const lastName = document.querySelector('#lname');
+function subButton() {
+  const nameInput = document.getElementById('fname').value;
+  console.log(nameInput);
+  const lastNameInput = document.getElementById('lname').value;
+  // console.log(lastNameInput);
+  const emailInput = document.getElementById('email').value;
+  // console.log(emailInput);
+  const numberInput = document.getElementById('number').value;
+  // console.log(numberInput);
+  const passwordInput = document.getElementById('password').value;
+  // console.log(passwordInput);
+  const cpasswordInput = document.getElementById('confirm').value;
+  // console.log(cpasswordInput);
+}
